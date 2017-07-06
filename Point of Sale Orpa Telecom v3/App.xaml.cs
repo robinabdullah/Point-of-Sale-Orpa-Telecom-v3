0206016343fc0f166ac5d914460f7d5e407de041
robinabdullah <robinabdullah>
2017-07-06 22:28:36 +0600

    Add project files.

---
+++ Point of Sale Orpa Telecom v3/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Point_of_Sale_Orpa_Telecom_v3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
